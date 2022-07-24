2c9b408fd4e78e1960b3ecb04b9365557c68caad
Documenter.jl <documenter@juliadocs.github.io>
2022-07-24 18:18:04 +0000

    build based on 2a9b25f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SelfOrganizingMaps","category":"page"},{"location":"#SelfOrganizingMaps","page":"Home","title":"SelfOrganizingMaps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SelfOrganizingMaps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SelfOrganizingMaps]","category":"page"}]
+}
