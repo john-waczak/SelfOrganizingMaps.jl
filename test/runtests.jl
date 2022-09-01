49d69f7539897812f9e8b1b36d91870563c113ea
John Waczak <john.louis.waczak@gmail.com>
2022-08-31 20:50:30 -0500

    Removing testing dependency on DataFrames and Plots per #8

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,8 @@
 using SelfOrganizingMaps
 using Test
-using Plots
 using Distances
 using MLJBase
 using StableRNGs  # should add this for later
-using DataFrames
 using MLJTestIntegration
 
 stable_rng() = StableRNGs.StableRNG(1234)
@@ -97,69 +95,4 @@ end
     @test summary[1].operations == "transform"
 end
 
-function SOMtoRGB(som::SOM)
-    SOM_pretty = [RGB(som.W[1,i], som.W[2,i], som.W[3,i]) for i∈1:size(som.W, 2)]
-end
 
-function plotSquareSOM(som::SOM)
-    cs = SOMtoRGB(som) # save this for before and after
-    p = scatter(som.coords[1,:], som.coords[2,:];
-                xlims=(-0.05, 1.05),
-                ylims=(-0.05, 1.05),
-                color=cs,
-                marker=:rect,
-                 ms=12.0,
-                aspect_ratio=:equal,
-                label=""
-                )
-    return p
-end
-
-function plotHexSOM(som::SOM)
-    cs = SOMtoRGB(som) # save this for before and after
-    p = scatter(som.coords[1,:], som.coords[2,:];
-                #xlims=(-0.05, 1.05),
-                #ylims=(-0.05, 0.85),
-                color=cs,
-                marker=:hexagon,
-                #ms=6.0,
-                ms=9.0,
-                aspect_ratio=:equal,
-                label="",
-                ticks=nothing,
-                xguide="",
-                yguide="",
-                framestyle=:none,
-                dpi=600,
-                )
-    return p
-end
-
-
-df = DataFrame()
-df.r = [1.0, 0.0, 0.0]
-df.g = [0.0, 1.0, 0.0]
-df.b = [0.0, 0.0, 1.0]
-
-
-model = SelfOrganizingMap()
-model.k = 25
-model.η = 0.20
-model.σ² = 0.2^2
-model.topology = :hexagonal
-#model.matching_distance = cosine_dist
-#model.matching_distance = chebyshev
-model.Nepochs=100
-
-
-m = machine(model, df)
-fit!(m)
-
-m.fitresult
-df_out = DataFrame(MLJBase.transform(m, df))
-
-
-plotHexSOM(m.fitresult)
-
-savefig("SOM_demo.png")
-savefig("SOM_demo.svg")
